b33aea4c224cf1445933cf8899f3443b78d4e728
dasilva333 <richard.pinedo@yahoo.com>
2012-03-11 12:16:27 -0400

    javascript to coffeescript conversion

--- coffee/application.coffee
+++ coffee/application.coffee
@@ -1,3 +1,104 @@
+window.RemoteLogger = 
+  logEvent: ->
+    
+window.PPL = new (->
+  
+  ##these are placeholder objects that must be here for playlist.com's api
+  @user = {}
+  @player = 
+    playWhenReady: ->
+
+  @search = new (->
+    ##This function gets called automatically due to the Playlist.com API
+    @searchResultsFn = ->
+    @searchVersion = ""
+    @searchTerm = ""
+    @trackdata = null
+    @currentPage = 1
+  )()
+  
+  false
+)() 
+
+Content = Backbone.View.extend
+  initialize: ->
+      @render()
+      @
+  
+  render: ->
+    @renderScrollbar()
+    ##@updateScrollbar()
+    $(window, "#content-scroll").on("resize", @updateScrollbar)
+  
+  renderScrollbar: ->
+      b = $("#content-scroll")
+      c = $("#content-view")
+      c.innerscroll
+          destination: b
+          draggable: true
+      scrollbar = b.children().not(c)
+      scrollbar.addClass("scrollbar")
+
+      c = ( ->
+          g = $('<div style="width:50px;height:50px;overflow:hidden;position:absolute;top:-200px;left:-200px;"><div style="height:100px;"></div>')
+          $("body").append(g)
+          f = g.children("div").innerWidth()
+          g.css("overflow-y", "scroll")
+          e = g.children("div").innerWidth()
+          g.remove()
+          (f - e)
+      )()
+      d = 155
+      b = 338
+      $('<style type="text/css">#content-scroll.scroll.open #content {right:' + (b - c) + "px}#content-scroll.scroll #content {right:" + (d - c) + "px}</style>").appendTo("head")
+      
+  
+  _updateScrollbar: ->
+      d = $("#content").innerHeight()
+      c = $("#content-scroll")
+      b = c.height()
+      c.removeClass("scroll")
+      if (b < d)
+          c.addClass("scroll")
+  
+  updateScrollbar: =>
+      setTimeout(@_updateScrollbar, 500)
+  
+
+ 
+SearchView = Backbone.View.extend
+    events: 
+        "submit": "doSearch"  
+   
+    doSearch: ( event ) ->
+        ##Button clicked, you can access the element that was clicked with event.currentTarget
+        alert( "Search for " + this.el.find("input").val() )
+        false
+
+Sidebar = Backbone.View.extend
+  speed: 200
+  initialize: -> 
+    @el = $(@options.el)
+    @content = $(@options.content)
+    @arrow = $(@options.arrow)
+    @items = $(@options.items)
+    @
+  
+  events: 
+      'click #collapsible-nav': 'toggleExpand'
+
+  toggleExpand: (event, show, speed) ->
+    if (show or this.items.is(":visible"))
+      this.el.animate({width:30}, speed || this.speed)
+      this.items.fadeOut(30)
+      this.arrow.switchClass('arrowLeft','arrowRight',30)
+      this.content.animate({left:31}, speed || this.speed)
+    else 
+      this.el.animate({width:155}, speed || this.speed)
+      this.items.delay(170).fadeIn(100)
+      this.arrow.switchClass('arrowRight','arrowLeft',30)
+      this.content.animate({left:156}, speed || this.speed)
+ 
 #
 # Some helper methods
 #
@@ -71,7 +172,6 @@ class TrackCollection extends Backbone.Collection
   
 class SearchResultsView extends Backbone.View
   
-  
   constructor: ->
     super
     
@@ -93,7 +193,7 @@ class SearchResultsView extends Backbone.View
     # A hacky way of reapplying the jquery mobile styles
     app.reapplyStyles(@el)     
 
-class HomeController extends Backbone.Router
+class AppController extends Backbone.Router
   routes :
     "home"  : "home"
     "search-:keyword" : "search"
@@ -114,10 +214,17 @@ class HomeController extends Backbone.Router
   show: (cid) ->
     ##@_views["venues-#{cid}-edit"] ||= new EditVenueView { model : Venues.getByCid(cid) }
     
-app.homeController = new HomeController()
+app.homeController = new AppController()
     
 $(document).ready ->
   ##Backbone.history.start()
-  app.homeController.search("Muse")    
+  app.homeController.search("Muse")   
+  
+  sb = new Sidebar({ el: "#nav", content: "#content-scroll", arrow: "#arrow", items: "#nav-content" });
+  sb.toggleExpand(null, false, 0);  
+  content = new Content();
+  search = new SearchView({ el: "#search-form" });
+  
+   
     
 @app = app
--- js/application.js
+++ js/application.js
@@ -1,6 +1,116 @@
 (function() {
-  var HomeController, SearchResultsView, Track, TrackCollection, app;
-  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; }, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  var AppController, Content, SearchResultsView, SearchView, Sidebar, Track, TrackCollection, app;
+  var _this = this, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; }, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  window.RemoteLogger = {
+    logEvent: function() {}
+  };
+
+  window.PPL = new (function() {
+    this.user = {};
+    this.player = {
+      playWhenReady: function() {}
+    };
+    this.search = new (function() {
+      this.searchResultsFn = function() {};
+      this.searchVersion = "";
+      this.searchTerm = "";
+      this.trackdata = null;
+      return this.currentPage = 1;
+    })();
+    return false;
+  })();
+
+  Content = Backbone.View.extend({
+    initialize: function() {
+      this.render();
+      return this;
+    },
+    render: function() {
+      this.renderScrollbar();
+      return $(window, "#content-scroll").on("resize", this.updateScrollbar);
+    },
+    renderScrollbar: function() {
+      var b, c, d, scrollbar;
+      b = $("#content-scroll");
+      c = $("#content-view");
+      c.innerscroll({
+        destination: b,
+        draggable: true
+      });
+      scrollbar = b.children().not(c);
+      scrollbar.addClass("scrollbar");
+      c = (function() {
+        var e, f, g;
+        g = $('<div style="width:50px;height:50px;overflow:hidden;position:absolute;top:-200px;left:-200px;"><div style="height:100px;"></div>');
+        $("body").append(g);
+        f = g.children("div").innerWidth();
+        g.css("overflow-y", "scroll");
+        e = g.children("div").innerWidth();
+        g.remove();
+        return f - e;
+      })();
+      d = 155;
+      b = 338;
+      return $('<style type="text/css">#content-scroll.scroll.open #content {right:' + (b - c) + "px}#content-scroll.scroll #content {right:" + (d - c) + "px}</style>").appendTo("head");
+    },
+    _updateScrollbar: function() {
+      var b, c, d;
+      d = $("#content").innerHeight();
+      c = $("#content-scroll");
+      b = c.height();
+      c.removeClass("scroll");
+      if (b < d) return c.addClass("scroll");
+    },
+    updateScrollbar: function() {
+      return setTimeout(_this._updateScrollbar, 500);
+    }
+  });
+
+  SearchView = Backbone.View.extend({
+    events: {
+      "submit": "doSearch"
+    },
+    doSearch: function(event) {
+      alert("Search for " + this.el.find("input").val());
+      return false;
+    }
+  });
+
+  Sidebar = Backbone.View.extend({
+    speed: 200,
+    initialize: function() {
+      this.el = $(this.options.el);
+      this.content = $(this.options.content);
+      this.arrow = $(this.options.arrow);
+      this.items = $(this.options.items);
+      return this;
+    },
+    events: {
+      'click #collapsible-nav': 'toggleExpand'
+    },
+    toggleExpand: function(event, show, speed) {
+      if (show || this.items.is(":visible")) {
+        this.el.animate({
+          width: 30
+        }, speed || this.speed);
+        this.items.fadeOut(30);
+        this.arrow.switchClass('arrowLeft', 'arrowRight', 30);
+        return this.content.animate({
+          left: 31
+        }, speed || this.speed);
+      } else {
+        this.el.animate({
+          width: 155
+        }, speed || this.speed);
+        this.items.delay(170).fadeIn(100);
+        this.arrow.switchClass('arrowRight', 'arrowLeft', 30);
+        return this.content.animate({
+          left: 156
+        }, speed || this.speed);
+      }
+    }
+  });
 
   app = {
     activePage: function() {
@@ -117,28 +227,28 @@
 
   })();
 
-  HomeController = (function() {
+  AppController = (function() {
 
-    __extends(HomeController, Backbone.Router);
+    __extends(AppController, Backbone.Router);
 
-    HomeController.prototype.routes = {
+    AppController.prototype.routes = {
       "home": "home",
       "search-:keyword": "search",
       "search-:keyword-:page": "search",
       "track-:trackid": "show"
     };
 
-    function HomeController() {
-      HomeController.__super__.constructor.apply(this, arguments);
+    function AppController() {
+      AppController.__super__.constructor.apply(this, arguments);
       this._views = {};
     }
 
-    HomeController.prototype.home = function() {
+    AppController.prototype.home = function() {
       var _base;
       return (_base = this._views)['home'] || (_base['home'] = new HomeView);
     };
 
-    HomeController.prototype.search = function(keyword, page) {
+    AppController.prototype.search = function(keyword, page) {
       var _base, _name;
       page || (page = 1);
       return (_base = this._views)[_name = "search-" + keyword + "-" + page] || (_base[_name] = new SearchResultsView({
@@ -147,16 +257,28 @@
       }));
     };
 
-    HomeController.prototype.show = function(cid) {};
+    AppController.prototype.show = function(cid) {};
 
-    return HomeController;
+    return AppController;
 
   })();
 
-  app.homeController = new HomeController();
+  app.homeController = new AppController();
 
   $(document).ready(function() {
-    return app.homeController.search("Muse");
+    var content, sb, search;
+    app.homeController.search("Muse");
+    sb = new Sidebar({
+      el: "#nav",
+      content: "#content-scroll",
+      arrow: "#arrow",
+      items: "#nav-content"
+    });
+    sb.toggleExpand(null, false, 0);
+    content = new Content();
+    return search = new SearchView({
+      el: "#search-form"
+    });
   });
 
   this.app = app;
